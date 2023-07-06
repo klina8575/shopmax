159875cb2c6771d0368a0bed471dc081ac9ac5ce
klina8575 <klina8575@gmail.com>
2023-07-06 17:08:14 +0900

    up

--- src/main/java/com/shopmax/repository/ItemRepository.java
+++ src/main/java/com/shopmax/repository/ItemRepository.java
@@ -11,7 +11,8 @@ import org.springframework.data.repository.query.Param;
 import com.shopmax.constant.ItemSellStatus;
 import com.shopmax.entity.Item;
                                //<해당 repository에서 사용할 Entity, Entity클래스의 기본키 타입>
-public interface ItemRepository extends JpaRepository<Item, Long> {
+public interface ItemRepository extends JpaRepository<Item, Long>,
+                                        ItemRepositoryCustom{
 	//select * from item where item_nm = ?
 	List<Item> findByItemNm(String itemNm);
 	
