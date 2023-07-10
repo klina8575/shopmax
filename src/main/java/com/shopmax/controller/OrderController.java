b4769f17cd7ff232962b60caa81258e7a91e6d2b
klina8575 <klina8575@gmail.com>
2023-07-10 12:58:19 +0900

    up

--- src/main/java/com/shopmax/controller/OrderController.java
+++ src/main/java/com/shopmax/controller/OrderController.java
@@ -1,11 +1,59 @@
 package com.shopmax.controller;
 
+import java.security.Principal;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.shopmax.dto.OrderDto;
+import com.shopmax.service.OrderService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 
 @Controller
+@RequiredArgsConstructor
 public class OrderController {
+	private final OrderService orderService;
 	
+	@PostMapping(value = "/order")
+	public @ResponseBody ResponseEntity order(@RequestBody @Valid OrderDto orderDto,
+			BindingResult bindingResult, Principal principal) {
+		//Principal: 로그인한 사용자의 정보를 가져올 수 있다.
+		
+		if(bindingResult.hasErrors()) {
+			StringBuilder sb = new StringBuilder();
+			List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+			
+			for (FieldError fieldError : fieldErrors) {
+				sb.append(fieldError.getDefaultMessage()); //에러메세지를 합친다.
+			}
+			
+			return new ResponseEntity<String>(sb.toString(), HttpStatus.BAD_REQUEST);
+		}
+		
+		String email = principal.getName(); //id에 해당하는 정보를 가지고 온다(email)
+		Long orderId;
+		
+		try {
+			orderId = orderService.order(orderDto, email); //주문하기
+		} catch (Exception e) {
+			return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
+		}
+		
+		return new ResponseEntity<Long>(orderId, HttpStatus.OK); //성공시
+	}
+	
+	
+	//주문내역을 보여준다
 	@GetMapping(value = "/orders")
 	public String orderHist() {
 		return "order/orderHist";
